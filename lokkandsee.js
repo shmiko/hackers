177ce8c32499d3dfa67529584a3bbae994fc3688
shmiko <shmiko@gmail.com>
2016-07-03 23:18:34 +1000

    looandsee

--- lokkandsee.js
+++ lokkandsee.js
@@ -82,3 +82,35 @@ var num = "1";
 for (var i = 10; i > 0; i--) {
     num = LookAndSay(num);
 }
+
+
+function lookSay(digits) {
+    var result = '',
+        chars = (digits + ' ').split(''),
+        lastChar = chars[0],
+        times = 0;
+ 
+    chars.forEach(function(nextChar) {
+        if (nextChar === lastChar) {
+            times++;
+        }
+        else {
+            result += (times + '') + lastChar;
+            lastChar = nextChar;
+            times = 1;
+        }
+    });
+ 
+    return result;
+}
+ 
+(function output(seed, iterations) {
+    for (var i = 0; i < iterations; i++) {
+        console.log(seed);
+        seed = lookSay(seed);
+    }
+})("1", 10);
+
+
+
+lookSay("33332211");
